5e947a360a23b242bf8bae1a1c9b624929cf56da
Jim Hill <jim@dammitjim.co.uk>
2019-01-08 00:49:25 +0000

    Committing what Ive got

--- src/controllers.ts
+++ src/controllers.ts
@@ -21,34 +21,55 @@ export async function urlVerificationController(req: Request, res: Response) {
   });
 }
 
-export async function linkSharedController(req: Request, res: Response) {
+interface PullRequestResult {
+  pullRequest: PullRequest;
+  result: any;
+}
+
+interface PullRequestAPIParts {
+  owner: string;
+  repo: string;
+  number: string;
+}
+
+// This function is bad and I should feed bad
+// TODO refactor
+const convertPRToParts = (link: string): PullRequestAPIParts => {
+  const [base, slashyBois, domain, owner, repo, method, number] = link.split(
+    "/"
+  );
+  return { owner, repo, number };
+};
+
+export const linkSharedController = async (req: Request, res: Response) => {
   res.json({ message: "ok" });
 
   const body = req.body as LinkSharedBody;
+
+  // save all the links to the database
+  // get the information for all of them on from github
+  // enrich saved database link with github data
   // we don't wait for this to process as it does not affect our response
+  // TODO refactor this into functions
   Promise.all(
     body.event.links.map(link => {
       return saveLink(link, body.event.user);
     })
-  ).then((values: PullRequest[]) => {
-    values.forEach(async pullRequest => {
+  )
+    .then((values: PullRequest[]) => {
       const github = req.app.get("github");
-      // TODO: this is bad but it is late
-      const [
-        base,
-        slashyBois,
-        domain,
-        owner,
-        repo,
-        method,
-        number
-      ] = pullRequest.link.split("/");
-      const result = await github.pulls.get({ owner, repo, number });
-      console.log(result.data);
-      console.log(result.status);
-    });
-  });
-}
+      return Promise.all(
+        values.map(
+          async (pullRequest): Promise<PullRequestResult> => {
+            const pullsParts = convertPRToParts(pullRequest.link);
+            const result = await github.pulls.get(pullsParts);
+            return { pullRequest, result };
+          }
+        )
+      );
+    })
+    .then((pullRequests: PullRequestResult[]) => {});
+};
 
 export async function invalidEventController(req: Request, res: Response) {
   res.status(400);
